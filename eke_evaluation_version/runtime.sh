880d7d821f6d17f273f3743214241b71240933db
Kimi Goyal <kimigoyal1701@gmail.com>
2024-02-29 13:57:47 +0000

    change runtime script

--- eke_evaluation_version/runtime.sh
+++ eke_evaluation_version/runtime.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-TIMEFORMAT='%R seconds.'
+TIMEFORMAT='%R'
 
-
-{ time python3 client.py negotiate --user=dummy_user --passwd=dummy_pwd; } 2>>runtime_2048.txt
+for n in {1..10};
+do
+time=$( { time python3 client.py negotiate --user=dummy_user --passwd=dummy_pwd; } 2>&1 )
+# timeVAL=$(echo "$time" | grep real | awk '{print $2}')
+timeVAL=$(echo "$time" | tail -n 1)
+echo "$n,$timeVAL" >> runtime_2048.csv
+done
